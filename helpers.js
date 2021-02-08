573334cb2d06c78f5ace08e7ea87189ef8ad4595
Ajinkya Meshram <ajinkya.meshram@gmail.com>
2021-02-08 20:52:36 +0530

    changed mail api to mailjet

--- helpers.js
+++ helpers.js
@@ -1,26 +1,60 @@
 const sgMail = require('@sendgrid/mail');
+const mailjet = require ('node-mailjet')
+.connect(process.env.MAILJET_API_KEY, process.env.MAILJET_API_SECRET);
 
 const sendVerificationMail = async (to, token, req, res) => {
-    const msg = {
-        to: to,
-        from: 'verification@travelplanner.com',
-        subject: 'Please verify your email',
-        html: `<body>
-                <h3> Please click the button below to verify your email <h3>
-                <br>
-                <hr>
-                <a href="http://localhost:5000/api/auth/signup/verify?t=${token}">VERIFY</a>
-            </body>`
-      };
+    // const msg = {
+    //     to: to,
+    //     from: 'verification@travelplanner.com',
+    //     subject: 'Please verify your email',
+    //     html: `<body>
+    //             <h3> Please click the button below to verify your email <h3>
+    //             <br>
+    //             <hr>
+    //             <a href="http://localhost:5000/api/auth/signup/verify?t=${token}">VERIFY</a>
+    //         </body>`
+    //   };
     
-    await sgMail.send(msg, (err, result) => {
-        if(err) {
-            console.log("there was an error sending mail", err.message);
-            throw err;
+    // await sgMail.send(msg, (err, result) => {
+    //     if(err) {
+    //         console.log("there was an error sending mail", err.message);
+    //         throw err;
 
-        }
-        else console.log(`mail sent to ${to}`);
-    });
+    //     }
+    //     else console.log(`mail sent to ${to}`);
+    // });
+
+    const request = mailjet
+        .post("send", {
+            'version': 'v3.1'
+        })
+        .request({
+            "Messages": [{
+                "From": {
+                    "Email": "ajinkya.meshram@gmail.com",
+                    "Name": "Travel Planner"
+                },
+                "To": [{
+                    "Email": to,
+                }],
+                "Subject": "Travel-Planner account verification",
+                "TextPart": "My first Mailjet email",
+                "HTMLPart": `<body>
+                                <h3> Please click the button below to verify your email <h3>
+                                <br>
+                                <hr>
+                                <a href="http://localhost:5000/api/auth/signup/verify?t=${token}">VERIFY</a>
+                            </body>`,
+            }]
+        })
+    request
+        .then((result) => {
+            console.log(result.body)
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
+
 }
 
 const checkSignedIn = (req, res, next) => {
